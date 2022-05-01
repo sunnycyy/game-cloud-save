869754e0ab4c07aa1871d40d33895680952f2e8d
Sunny <yuk.yat.cheng@gmail.com>
2022-05-02 02:32:36 +0900

    Update assert lib

--- lambda/lib/assert-lib.ts
+++ lambda/lib/assert-lib.ts
@@ -1,7 +1,15 @@
-import {strict as assert} from "assert";
-
 export function assertDefined(map: Record<string, any>): void {
     for (const [key, value] of Object.entries(map)) {
-        assert.notEqual(value, undefined, `VALUE_UNDEFINED: ${key}`);
+        if (value === undefined) {
+            throw `VALUE_UNDEFINED: ${key}`;
+        }
+    }
+}
+
+export function assertNotEmpty(map: Record<string, Array<any>>): void {
+    for (const [key, arr] of Object.entries(map)) {
+        if (arr.length <= 0) {
+            throw `ARRAY_EMPTY: ${key}`;
+        }
     }
 }
