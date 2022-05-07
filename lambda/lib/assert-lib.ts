1a476460902b48c751cabe437e854cc8aaa34dff
Sunny <yuk.yat.cheng@gmail.com>
2022-05-08 02:07:08 +0900

    Add assert exist

--- lambda/lib/assert-lib.ts
+++ lambda/lib/assert-lib.ts
@@ -6,6 +6,14 @@ export function assertDefined(map: Record<string, any>): void {
     }
 }
 
+export function assertExist(map: Record<string, any>): void {
+    for (const [key, value] of Object.entries(map)) {
+        if (value === undefined) {
+            throw `VALUE_NOT_EXIST: ${key}`;
+        }
+    }
+}
+
 export function assertNotEmpty(map: Record<string, Array<any>>): void {
     for (const [key, arr] of Object.entries(map)) {
         if (arr.length <= 0) {
